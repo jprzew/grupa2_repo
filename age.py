c462bdebe119a685afd8b098a593c03f5b231112
Janusz Przewocki <janusz.przewocki@ug.edu.pl>
2024-12-10 13:50:22 +0100

    Added some more tests

--- age.py
+++ age.py
@@ -1,4 +1,8 @@
 def categorize_by_age(age):
+
+    if not isinstance(age, (int, float)):
+        raise ValueError('Invalid value: age must be int or float.')
+
     if 0 <= age <= 10:
         return 'Child'
     elif 10 < age <= 19:
--- test_age.py
+++ test_age.py
@@ -5,4 +5,34 @@ from age import categorize_by_age
 class TestCategorizeByAge(unittest.TestCase):
 
     def test_child(self):
-        self.assertEquals(categorize_by_age(5), 'Child')
+        self.assertEqual(categorize_by_age(5), 'Child')
+
+    def test_teenager(self):
+        self.assertEqual(categorize_by_age(15), 'Teenager')
+
+    def test_adult(self):
+        self.assertEqual(categorize_by_age(30), 'Adult')
+
+    def test_senior(self):
+        self.assertEqual(categorize_by_age(70), 'Senior')
+
+    def test_negative_age(self):
+        self.assertEqual(categorize_by_age(-5), 'Invalid age: -5.')
+
+    def test_invalid_value(self):
+
+        with self.assertRaises(ValueError):
+            categorize_by_age('Hello')
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
+
+# Oprócz unittest.skip mamy jeszcze:
+# - unittest.skipIf(condition, reason)
+#   np. condition = sys.version_info < (3, 3)
+#
+# - unittest.skipUnless(condition, reason)
+
+
+# Tutaj sobie wypiszemy inne funkcje z grupy "assert"
