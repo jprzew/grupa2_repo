0221cc621f609cd5669610b2718d46b3e13586e0
jprzew <janusz.przewocki@ug.edu.pl>
2025-01-13 09:39:01 +0100

    Created script to download the data

---
+++ src/config.py
@@ -0,0 +1,9 @@
+COLUMN_NAMES = ['sepal_length',
+                'sepal_width' ,
+                'petal_length',
+                'petal_width']
+
+TARGET = 'species'
+
+INPUT_FILE = 'iris.csv'
+DATA_PATH = 'data'
--- src/download.py
+++ src/download.py
@@ -1,19 +1,23 @@
 from sklearn import datasets
 import pandas as pd
+from utils import get_repo_path
+import config as cfg
 
 
-COLUMN_NAMES = ['sepal_length',
-                'sepal_width' ,
-                'petal_length',
-                'petal_width']
+def main():
+    iris = datasets.load_iris()
 
-iris = datasets.load_iris()
+    # Create dataframe
+    df = pd.DataFrame(iris['data'])
+    df.columns = cfg.COLUMN_NAMES
 
-df = pd.DataFrame(iris['data'])
-df.columns = COLUMN_NAMES
+    # Add column with species' names
+    df[cfg.TARGET] = iris['target']
+    target_names = {index: name for index, name in enumerate(iris['target_names'])}
+    df[cfg.TARGET] = df[cfg.TARGET].replace(target_names)
 
-print(df)
-# print(type(iris))
-# print(iris['target_names'])
+    df.to_csv(get_repo_path() / cfg.DATA_PATH / cfg.INPUT_FILE, index=False)
 
 
+if __name__ == '__main__':
+    main()
